0809225ee5244b7eaf54c619248096c5c9ace7ea
Tetiana Taratula <ttaratula@gmail.com>
2025-08-06 21:13:31 +0100

    user

--- src/services/auth.js
+++ src/services/auth.js
@@ -2,10 +2,8 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import createError from 'http-errors';
 import createHttpError from 'http-errors';
-import User from '../models/User.js';
+import User from '../models/user.js';
 import Session from '../models/session.js';
-// import { User } from '@models/User.js';
-// import { Session } from '@models/session.js';
 
 import dotenv from 'dotenv';
 dotenv.config();
