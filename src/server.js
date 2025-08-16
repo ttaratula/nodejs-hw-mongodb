f90bc6bc3a083edc78d8dc51c9d3156c34d64e58
Tetiana Taratula <ttaratula@gmail.com>
2025-08-16 13:34:55 +0100

    hw7 new

--- src/server.js
+++ src/server.js
@@ -3,6 +3,11 @@
 // import logger from 'pino-http';
 // import dotenv from 'dotenv';
 // import cookieParser from 'cookie-parser';
+// import path from 'path';
+// import { fileURLToPath } from 'url';
+// import swaggerUi from 'swagger-ui-express';
+// import YAML from 'yamljs';
+
 // import router from './routers/index.js';
 // import { errorHandler } from './middlewares/errorHandler.js';
 // import { notFoundHandler } from './middlewares/notFoundHandler.js';
@@ -10,16 +15,23 @@
 
 // dotenv.config();
 
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
 // export function setupServer() {
 //   const app = express();
+
 //   app.use(cors());
 //   app.use(cookieParser());
 //   app.use(express.json());
 //   app.use('/uploads', express.static(UPLOAD_DIR));
-
 //   app.use(logger());
 
 //   const PORT = process.env.PORT;
+
+//   // Swagger setup
+//   const swaggerDocument = YAML.load(path.resolve('./src/docs/openapi.yaml'));
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 //   app.get('/', (req, res) => {
 //     req.log.info('Request received at /');
@@ -43,7 +55,6 @@ import logger from 'pino-http';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import path from 'path';
-import { fileURLToPath } from 'url';
 import swaggerUi from 'swagger-ui-express';
 import YAML from 'yamljs';
 
@@ -54,34 +65,34 @@ import { UPLOAD_DIR } from './constants/index.js';
 
 dotenv.config();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 export function setupServer() {
   const app = express();
 
+  // Middleware
   app.use(cors());
   app.use(cookieParser());
   app.use(express.json());
   app.use('/uploads', express.static(UPLOAD_DIR));
   app.use(logger());
 
-  const PORT = process.env.PORT;
-
   // Swagger setup
-  const swaggerDocument = YAML.load(path.join(__dirname, './docs/openapi.yaml'));
+  const swaggerDocument = YAML.load(path.resolve('./src/docs/openapi.yaml'));
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+  // Test route
   app.get('/', (req, res) => {
     req.log.info('Request received at /');
     res.send('hello world');
   });
 
+  // Routers
   app.use(router);
 
+  // Error handlers
   app.use(notFoundHandler);
   app.use(errorHandler);
 
+  const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
