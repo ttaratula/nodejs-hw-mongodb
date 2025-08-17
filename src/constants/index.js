b1f1e91d759688f40df36723597cd39c35e95875
Tetiana Taratula <ttaratula@gmail.com>
2025-08-17 13:18:33 +0100

    hw fixed

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,7 +2,7 @@ import path from 'node:path';
 
 export const UPLOAD_DIR = path.join(process.cwd(), 'uploads');
 
-export const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'openapi.yaml');
+export const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'swagger.json');
 
 export const SORT_ORDER = {
   ASC: 'asc',
--- src/middlewares/swaggerDocs.js
+++ src/middlewares/swaggerDocs.js
@@ -1,17 +1,15 @@
 import createHttpError from 'http-errors';
 import swaggerUI from 'swagger-ui-express';
+import fs from 'node:fs';
 import { SWAGGER_PATH } from '../constants/index.js';
-import YAML from 'yamljs';
 
 export const swaggerDocs = () => {
   try {
-    const swaggerDoc = YAML.load(SWAGGER_PATH);
-    return { serve: swaggerUI.serve, setup: swaggerUI.setup(swaggerDoc) };
+    const swaggerDoc = JSON.parse(fs.readFileSync(SWAGGER_PATH).toString());
+    return [...swaggerUI.serve, swaggerUI.setup(swaggerDoc)];
   } catch (err) {
     console.error("Swagger load error:", err.message);
-    return {
-      serve: (req, res, next) => next(createHttpError(500, "Can't load swagger docs")),
-      setup: (req, res, next) => next(createHttpError(500, "Can't load swagger docs")),
-    };
+    return (req, res, next) =>
+      next(createHttpError(500, "Can't load swagger docs"));
   }
 };
